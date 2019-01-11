28013a024779e173f187960949370de7953b0bf3
kimnura <mail>
2019-01-11 17:38:58 +0900

    Hello World! のTextViewを削除。

--- app/src/main/java/jp/techacademy/kimura/hirohisa/myapplication/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kimura/hirohisa/myapplication/MainActivity.kt
@@ -8,5 +8,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var human1 = Human("human 1", 25, "盆栽")
+        var human2 = Human("human 2", 55, "掃除")
+
+        human1.say()
+        human1.think()
+
+        human2.say()
+        human2.think()
+
+
     }
+
 }
---
+++ app/src/main/java/jp/techacademy/kimura/hirohisa/myapplication/Thinkable.kt
@@ -0,0 +1,7 @@
+package jp.techacademy.kimura.hirohisa.myapplication
+
+interface Thinkable {
+
+    fun think()
+
+}
